95363c82a7bc813ad0cb04870a92b1f3aa6caf15
loki <gunlinux@ya.ru>
2024-02-21 03:20:03 +1000

    basic rescaller

--- hash.py
+++ hash.py
@@ -45,6 +45,10 @@ class HashNode:
         if self.sublist:
             return [item[1] for item in self.sublist]
 
+    def items(self):
+        if self.sublist:
+            return [item for item in self.sublist]
+
     def __str__(self):
         values = ",".join([x[0] for x in self.sublist])
         return f"({len(self.sublist)}) {values}>"
@@ -54,18 +58,23 @@ class HashNode:
 
 
 class MyHash:
-    def __init__(self, max_size=64):
+    def __init__(self, max_size=64, treshhold=0.7):
         self.__data: list = [None] * max_size
         self.__filled: int = 0
         self.__size: int = max_size
+        self.__treshhold = treshhold
 
-    def add(self, key, value):
-        index = self.__hashfunc(key)
-        if self.__data[index] is None:
-            self.__data[index] = HashNode()
-            self.__filled += self.__data[index].add_value(key, value)
-            return
-        self.__filled += self.__data[index].add_value(key, value)
+    def add(self, key, value, data=None):
+        if self.__overload__() and not data:
+            self.__resize__()
+
+        if not data:
+            data = self.__data
+
+        index = self.__hashfunc(key, len(data))
+        if data[index] is None:
+            data[index] = HashNode()
+        self.__filled += data[index].add_value(key, value)
 
     def seti(self, key, value):
         index = self.__hashfunc(key)
@@ -83,8 +92,10 @@ class MyHash:
         self.__filled -= c
         return val
 
-    def __hashfunc(self, key):
-        return self.__hashcalc(key) % self.__size
+    def __hashfunc(self, key, size=None):
+        if size is None:
+            return self.__hashcalc(key) % self.__size
+        return self.__hashcalc(key) % size
 
     def __hashcalc(self, key):
         hash = hashlib.md5(key.encode()).hexdigest()
@@ -99,14 +110,29 @@ class MyHash:
     def values(self):
         return [x for c in self.__data if c is not None for x in c.values()]
 
-    def __rehash__(self):
-        pass
+    def items(self):
+        return [x for c in self.__data if c is not None for x in c.items()]
 
     def __resize__(self):
+        new_size = self.__size * 2
+        new_data: list = [None] * new_size
+
+        old_filled = self.__filled
+
+        for k, v in self.items():
+            self.add(key=k, value=v, data=new_data)
+        self.__data = new_data
+        self.__size = new_size
+
+        self.__filled = old_filled
+        print('resize is over')
         pass
 
     def __len__(self):
         return self.__filled
+
+    def __overload__(self):
+        return (self.__filled / self.__size) > self.__treshhold
 
     def debug(self):
         for el in self.__data:
